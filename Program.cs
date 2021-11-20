b651071e814dff1e11255c6eec0e07da630eb957
Luna Wen <lunawen33@gmail.com>
2021-11-20 17:31:58 +1100

    route variable constraints

--- Program.cs
+++ Program.cs
@@ -9,4 +9,11 @@ app.MapGet("/todos", () => new { TodoItem = "Learn about routing", Completed = f
 // route variables
 app.MapGet("/hello/{name}", (string name) => $"Hi, {name}");
 
+// specify variable type - add constraint
+// used for disambiguate similar routes, not for input validation
+// returns 404 if not match the type
+app.MapGet("/todos/{id:int}", (int id) => $"Todo id: {id}");
+app.MapGet("/todos/{active:bool}", (bool active) => $"Todo is active: {active}");
+app.MapGet("/todos/{name}", (string name) => $"Todo name: {name}");
+
 app.Run();
