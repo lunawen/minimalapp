4745dd15f4a31e7da2904ff1d64edbd6a604b77a
Luna Wen <lunawen33@gmail.com>
2021-11-20 17:19:11 +1100

    add more routes

--- Program.cs
+++ Program.cs
@@ -3,4 +3,10 @@ var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 
+// add another route
+app.MapGet("/todos", () => new { TodoItem = "Learn about routing", Completed = false });
+
+// route variables
+app.MapGet("/hello/{name}", (string name) => $"Hi, {name}");
+
 app.Run();
