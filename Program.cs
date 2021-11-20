6c5fcac4faaf4dfe2f1a8a7ef5b456cd83718a04
Luna Wen <lunawen33@gmail.com>
2021-11-20 18:22:38 +1100

    add swagger doc

--- Program.cs
+++ Program.cs
@@ -1,6 +1,21 @@
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Todo API", Description = "Keep track of your todos", Version = "v1" });
+});
+
+
 var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Todo API V1");
+});
 
 if (app.Environment.IsDevelopment())
 {
